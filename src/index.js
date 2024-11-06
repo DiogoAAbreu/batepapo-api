fffadfa37e22c5fe289577083f7b7ed056960e0b
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-11-06 16:15:21 -0300

    feat: adicionado verificação de inatividade no arquivo do servidor
    adicionado setIntervalo de 15 segundos, removendo todos usuarios que estejam inativos a mais de 10 segundos e salvando mensagem de deslog e removendo participante da lista de participantes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import { getParticipants, postParticipants } from './controllers/participants.js';
 import { getMessages, postMessage } from './controllers/messages.js';
 import { setStatus } from './controllers/status.js';
+import { db } from './connection.js';
+import dayjs from 'dayjs';
 
 const app = express();
 
@@ -18,6 +20,25 @@ app.post('/messages', postMessage);
 app.get('/messages', getMessages)
 
 app.post('/status', setStatus);
+
+setInterval(async () => {
+    const participants = await db.collection('participants').find({}).toArray();
+    participants.map(async (participant) => {
+        if ((Date.now() - participant.lastStatus) > 10000) {
+            const time = dayjs().format('HH:mm:ss');
+
+            const messageLogout = {
+                from: participant.name,
+                to: 'Todos',
+                text: 'sai da sala...',
+                type: 'status',
+                time
+            }
+            await db.collection('messages').insertOne(messageLogout);
+            await db.collection('participants').deleteOne({ _id: participant._id });
+        }
+    })
+}, 15000)
 
 app.listen(5000, () => {
     console.log('Run in localhost://5000')
